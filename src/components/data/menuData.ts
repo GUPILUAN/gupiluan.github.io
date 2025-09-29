0223185ac2dedb4761927e6ec3229ee50332c82c
Luis <gupiluan@hotmail.com>
2025-09-29 02:37:56 -0600

    chore: update dependencies and refactor import paths
    - Updated Astro dependency from ^5.2.5 to ^5.14.1 in package.json.
    - Refactored import paths in multiple components to use absolute paths for better readability and maintainability.
    - Adjusted imports in About, Footer, Logo, Portfolio, SocialIcons, Boton, and various other components to align with the new path structure.
    - Updated TypeScript configuration to include path aliases for components, layouts, pages, assets, and utilities.

--- src/components/data/menuData.ts
+++ src/components/data/menuData.ts
@@ -1,16 +1,16 @@
 interface MenuItem {
-	title: string;
-	icon: string;
+  title: string;
+  icon: string;
 }
 
 export const mainMenu: MenuItem[] = [
-	{ title: 'home', icon: 'lucide:home' },
-	{ title: 'about', icon: 'octicon:person-24' },
-	{ title: 'portfolio', icon: 'carbon:portfolio' },
+  { title: "home", icon: "lucide:home" },
+  { title: "about", icon: "octicon:person-24" },
+  { title: "portfolio", icon: "carbon:portfolio" },
 ];
 
 export const menuBlog: MenuItem[] = [
-	{ title: 'home', icon: 'lucide:home' },
-	{ title: 'about', icon: 'octicon:person-24' },
-	{ title: 'portfolio', icon: 'carbon:portfolio' },
+  { title: "home", icon: "lucide:home" },
+  { title: "about", icon: "octicon:person-24" },
+  { title: "portfolio", icon: "carbon:portfolio" },
 ];
